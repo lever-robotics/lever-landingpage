85f81d724db91569d194ee8aaae4c19c633700a3
Mark Soulier <mark.soulier@usu.edu>
2025-03-08 12:11:35 -0700

    Updates

--- content/debug.php
+++ content/debug.php
@@ -1,1 +1,24 @@
- 
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$dir = __DIR__;
+$files = scandir($dir);
+$fileList = [];
+
+foreach ($files as $file) {
+    if ($file != "." && $file != ".." && $file != "debug.php") {
+        $fileList[] = [
+            'name' => $file,
+            'size' => filesize($dir . '/' . $file),
+            'modified' => date("Y-m-d H:i:s", filemtime($dir . '/' . $file)),
+            'type' => pathinfo($file, PATHINFO_EXTENSION)
+        ];
+    }
+}
+
+echo json_encode([
+    'directory' => $dir,
+    'files' => $fileList
+], JSON_PRETTY_PRINT);
+?> 
